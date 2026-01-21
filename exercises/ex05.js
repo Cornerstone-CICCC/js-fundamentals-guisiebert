7cecc9a08b61a4825e24265117c4aa4fc1242d8f
Guilherme Siebert <guilhermebsiebert@gmail.com>
2026-01-21 13:19:17 -0800

    exercise 05

--- exercises/ex05.js
+++ exercises/ex05.js
@@ -26,3 +26,23 @@ var shirtSleeve = 8.47; // size M (medium)
 Then print N/A to the console because the measurements don't all match up with one particular size.
 
 */
+
+const shirtWidth = 20.2;
+const shirtLength = 29.5;
+const shirtSleeve = 8.39;
+
+if (shirtWidth >= 18 && shirtWidth < 20 && shirtLength >= 28 && shirtLength < 29 && shirtSleeve >= 8.13 && shirtSleeve < 8.38) {
+    console.log("S");
+} else if (shirtWidth >= 20 && shirtWidth < 22 && shirtLength >= 29 && shirtLength < 30 && shirtSleeve >= 8.38 && shirtSleeve < 8.63) {
+    console.log("M");
+} else if (shirtWidth >= 22 && shirtWidth < 24 && shirtLength >= 30 && shirtLength < 31 && shirtSleeve >= 8.63 && shirtSleeve < 8.88) {
+    console.log("L");
+} else if (shirtWidth >= 24 && shirtWidth < 26 && shirtLength >= 31 && shirtLength < 33 && shirtSleeve >= 8.88 && shirtSleeve < 9.63) {
+    console.log("XL");
+} else if (shirtWidth >= 26 && shirtWidth < 28 && shirtLength >= 33 && shirtLength < 34 && shirtSleeve >= 9.63 && shirtSleeve < 10.13) {
+    console.log("2XL");
+} else if (shirtWidth >= 28 && shirtWidth < 30 && shirtLength >= 34 && shirtLength < 35 && shirtSleeve >= 10.13 && shirtSleeve < 10.38) {
+    console.log("3XL");
+} else {
+    console.log("NA");
+}
