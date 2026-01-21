22069e1f2a788180400e53902151bd67ca88e893
Guilherme Siebert <guilhermebsiebert@gmail.com>
2026-01-21 12:20:33 -0800

    exercise 01

--- exercises/ex01.js
+++ exercises/ex01.js
@@ -6,12 +6,10 @@ Write an if…else statement that:
 Hint: Use the %(modulo) operator to determine is a number is even or odd.
 */
 
-const number = 3; // Change your number to test your if statement
+const number = 5; // Change your number to test your if statement
 
-if (/* your if condition goes here */) {
-    
+if (number % 2 === 0) {
+    console.log("even");
 } else {
-    
+    console.log("odd");
 }
-
-
