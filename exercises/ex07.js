26c36191f99d9e8f86d859496bce14d526e838d9
Guilherme Siebert <guilhermebsiebert@gmail.com>
2026-01-22 11:34:42 -0800

    solutions for ex07

--- exercises/ex07.js
+++ exercises/ex07.js
@@ -1,5 +1,6 @@
 /*
-In 2015, the U.S. Bureau of Labor Statistics conducted research to reveal how average salary is directly related to the number of years spent in school. In their findings, they found that people with:
+In 2015, the U.S. Bureau of Labor Statistics conducted research to reveal how average salary is directly related to the number of years spent in school. 
+In their findings, they found that people with:
 
 no high school diploma earned an average of $25,636/year,
 a high school diploma earned an average of $35,256/year,
@@ -14,10 +15,40 @@ Write a switch statement to set the average salary of a person based on their ty
 Afterwards, print the following to the console.
 
 In 2015, a person with __________ earned an average of __________/year.
-Fill in the blanks with the type of education and the expected average salary. Make sure to use correct grammar in your printed statement, and watch out for any extra or missing characters (including spaces and punctuation marks). For help, refer to the findings above.
+Fill in the blanks with the type of education and the expected average salary. 
+Make sure to use correct grammar in your printed statement, and watch out for any extra or missing characters 
+(including spaces and punctuation marks). For help, refer to the findings above.
 
 In 2015, a person with a Bachelor's degree earned an average of $59,124/year.
 
 TIP: To print out the average salary with commas (i.e. 59,124), use the toLocaleString() method and pass it the locale "en-US". For example, salary.toLocaleString("en-US"). 
 
 */
+
+let education = "Bachelor's degree";
+let salary = 0; 
+
+switch (education) {
+    case "high school diploma":
+        salary = 35256;
+        break;
+    case "Associate's degree":
+        salary = 41496;
+        break; 
+    case "Bachelor's degree":
+        salary = 59124;
+        break; 
+    case "Master's degree":
+        salary = 69732;
+        break;
+    case "Professional degree":
+        salary = 89960;
+        break;
+    case "Doctoral degree":
+        salary = 84396;
+        break;
+}
+
+let formattedSalary = salary.toLocaleString("en-US");
+
+console.log(`In 2015, a person with ${education} earned an average of ${formattedSalary}/year.`);
