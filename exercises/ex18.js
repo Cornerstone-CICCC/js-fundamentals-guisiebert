a967d0f35e150763208e88c313f7dc8bb7443435
Guilherme Siebert <guilhermebsiebert@gmail.com>
2026-01-23 11:52:57 -0800

    solution for ex18

--- exercises/ex18.js
+++ exercises/ex18.js
@@ -4,3 +4,9 @@ Write a named function expression that stores the function in a variable called 
 cry();
 Returns: boohoo!
 */
+
+const crySound = function cry() {
+    return "boohoo!";
+}
+
+console.log(crySound());
