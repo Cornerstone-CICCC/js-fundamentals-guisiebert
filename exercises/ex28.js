f54cd7124d438011d3d03577b0993a1cb8e1dcb4
Guilherme Siebert <guilhermebsiebert@gmail.com>
2026-01-27 12:08:48 -0800

    solution for ex28

--- exercises/ex28.js
+++ exercises/ex28.js
@@ -11,3 +11,31 @@ deleteMessage(index) - removes the message corresponding to the index provided
 addFriend() - increases the friend count by 1
 removeFriend() - decreases the friend count by 1
 */
+
+const facebookProfile = {
+  name: "Gui Siebert",
+  friends: 100,
+  messages: ["Hello world", "I'm Gui"],
+  postMessage: function(msg) {
+    this.messages.push(msg)
+  },
+  addFriend: function() {
+      this.friends = this.friends + 1
+  },
+  removeFriend: function() {
+    this.friends--
+  }
+}
+
+// test posting a message
+console.log(facebookProfile.messages)
+facebookProfile.postMessage("I hate social media")
+facebookProfile.postMessage("This is my last post. Goodbye world.")
+console.log(facebookProfile.messages)
+
+// test adding/removing friends
+console.log(facebookProfile.friends)
+facebookProfile.addFriend()
+console.log(facebookProfile.friends)
+facebookProfile.removeFriend()
+console.log(facebookProfile.friends)
